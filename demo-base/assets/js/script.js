709744db10c4f452883fbbb7aea97372696d86f8
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-12-09 14:03:20 -0500

    Changement

---
+++ demo-base/assets/js/script.js
@@ -0,0 +1,25 @@
+let monNombre1 = 10;
+let monNombre2 = 20;
+
+monNombre1 = monNombre1 - 1;
+monNombre2 = monNombre2 + 10;
+
+let total = monNombre1 + monNombre2;
+
+let produit = monNombre1 * monNombre2;
+let quotient = monNombre1 / monNombre2;
+
+// console.log(total, produit, quotient);
+
+// monNombre = monNombre1 + 1;
+// monNombre1 += 1;
+monNombre1++;
+monNombre1++;
+
+monNombre1 -= 1;
+monNombre1--;
+
+console.log(monNombre1);
+
+let monAgeEnTexte = "12";
+let monAgeEnNombre = Number(monAgeEnTexte)
